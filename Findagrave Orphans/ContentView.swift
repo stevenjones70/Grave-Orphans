b4fb763a8c9c0d105d345a2e00bd816ee902c950
Steven Jones <stevenjones69@me.com>
2026-04-25 12:47:47 -0400

    Initial Commit

---
+++ Findagrave Orphans/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Findagrave Orphans
+//
+//  Created by Steven Jones on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Findagrave Orphans/Findagrave_OrphansApp.swift
@@ -0,0 +1,17 @@
+//
+//  Findagrave_OrphansApp.swift
+//  Findagrave Orphans
+//
+//  Created by Steven Jones on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Findagrave_OrphansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
